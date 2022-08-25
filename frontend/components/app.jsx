b19ad3a692c7afd4d8263d44800aa035e485f970
Sing Cheng <Sing.cheng@outlook.com>
2022-08-25 15:20:38 -0400

    heroku

--- frontend/components/app.jsx
+++ frontend/components/app.jsx
@@ -2,7 +2,7 @@ import React from "react";
 import { Route, Switch } from "react-router-dom";
 import LoginFormContainer from "./session_form/login_form_container";
 import SignupFormContainer from "./session_form/signup_form_container";
-import { AuthRoute, ProtectedRoute } from '../util/route_util'
+import { AuthRoute} from '../util/route_util'
 import SplashContainer from './splash/splash_container';
 import BusinessShowContainer from "./business/business_show_container";
 import BusinessIndexContainer from "./business/business_index_container";
