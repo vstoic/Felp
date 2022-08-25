b6186010b07da72fa4678c3e8cbc7a28262c3b5a
Sing Cheng <Sing.cheng@outlook.com>
2022-08-25 15:33:07 -0400

    heroku

--- app/controllers/api/reviews_controller.rb
+++ app/controllers/api/reviews_controller.rb
@@ -1,44 +1,44 @@
-class Api::ReviewsController < ApplicationController
-  before_action :require_logged_in, only [:create, :update, :destroy]
+# class Api::ReviewsController < ApplicationController
+#   # before_action :require_logged_in, only [:create, :update, :destroy]
 
-  def index
-    @reviews = Review.all.where(business_id: params[:business_id])
-  end
+#   def index
+#     @reviews = Review.all.where(business_id: params[:business_id])
+#   end
 
-  def create
-    @review = current_user.reviews.new(review_params)
+#   def create
+#     @review = current_user.reviews.new(review_params)
 
-    if @review.save
-      render :show
-    else
-      render json: @review, status: :unprocessable_entity
-    end
-  end
+#     if @review.save
+#       render :show
+#     else
+#       render json: @review, status: :unprocessable_entity
+#     end
+#   end
 
-  def show 
-    @review = Review.find(review_params)
-    render :show
-  end
+#   def show 
+#     @review = Review.find(review_params)
+#     render :show
+#   end
 
-  def update
-    @review = Review.find(params[:id])
-    if @review.update(review_params)
-        render :show
-    else
-        render json: ["edit not complete"], status: 422
-    end
-  end
+#   def update
+#     @review = Review.find(params[:id])
+#     if @review.update(review_params)
+#         render :show
+#     else
+#         render json: ["edit not complete"], status: 422
+#     end
+#   end
 
-  def destroy
-    @review = review.find_by(id: params[:id])
-    if @review && @review.destroy
-    end
+#   def destroy
+#     @review = review.find_by(id: params[:id])
+#     if @review && @review.destroy
+#     end
 
-  end
+#   end
 
-  private
+#   private
 
-  def review_params
-    params.require(:review).permit(:review_body, :rating, :user_id, :business_id)
-  end
-end
+#   def review_params
+#     params.require(:review).permit(:review_body, :rating, :user_id, :business_id)
+#   end
+# end
