ae5f92a7914bdec14b544e211d076694a65b4ad1
Sing Cheng <Sing.cheng@outlook.com>
2022-11-14 17:49:59 -0500

    search working, needs css

--- frontend/components/search/search.jsx
+++ frontend/components/search/search.jsx
@@ -1,27 +1,31 @@
 import React from "react";
-import {withRouter, Redirect, Link} from 'react-router-dom';
+import { Link } from 'react-router-dom';
 
 class Search extends React.Component {
     constructor(props) {
         super(props)
         this.state = {
-            user: this.props.currentUser,
+            // user: this.props.currentUser,
+            businesses: this.props.business,
             searchInput: "",
             results: [],
         };
         this.handleUpdate = this.handleUpdate.bind(this);
     };
 
+
     handleUpdate(e) {
         let restultsArr = [];
-        this.setState({ searchInput: e.target.value })
+        this.setState({ searchInput: e.currentTarget.value })
         if (e.currentTarget.value) {
-            object.values(this.props.business).forEach(business => {
-                let businessName = businessName.toLowerCase();
-                if (business.includes(e.currentTarget.value.toLowerCase())) {
+            Object.values(this.props.businesses).forEach(business => {
+                // console.log(business)
+                let businessName = business.name;
+                if (businessName.toLowerCase().includes(e.currentTarget.value.toLowerCase())) {
                     restultsArr.push(business)
                 }
         })
+
         this.setState({ results: restultsArr })
         };
     };
@@ -36,7 +40,8 @@ class Search extends React.Component {
     };
 
     render() {
-        // console.log(this.props.businesses)
+        // console.log(this.props)
+        // console.log(searchedResults)
         return (
             <div className="search-main-container">
                 <input type="text" placeholder="SearchBar YUR" 
@@ -45,11 +50,11 @@ class Search extends React.Component {
                 className="search-input"/>
                 <div className="search-results">
                     {this.state.results.map(searchedResults => {
-                        return <link to={`/businesses/${searchedResults.id}`} key={searchedResults.id}>
-                            <div className="result-name">
+                        return <Link to={`/business/${searchedResults.id}`} key={searchedResults.id}>
+                            <p className="result-name">
                                 {searchedResults.name}
-                            </div>
-                        </link>
+                            </p>
+                        </Link>
                     })}
                 </div>
             </div>
