765f2676e97b2097b53d7d1039816bac629aaec4
Sing Cheng <Sing.cheng@outlook.com>
2023-01-30 12:36:08 -0500

    renaming folders

--- frontend/reducers/errors_reducer.js
+++
@@ -1,7 +0,0 @@
-import { combineReducers } from "redux";
-import sessionErrorsReducer from "./session_errors_reducer";
-
-const ErrorsReducer = combineReducers({
-    session: sessionErrorsReducer
-});
-export default ErrorsReducer;
